f59966226a32e726a26dbe4a3196b3e95d5c8877
Uttiya Pal <iamuttiyapal@gmail.com>
2026-06-19 02:12:58 +0530

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+var direction: Vector2 = Vector2(1,1)
+var speed = 4000
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("left","right","up","down")
+	velocity = direction * speed * delta
+	animation()
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("confirm"):
+		print('something pressed')
+		
+func animation():
+		if direction:
+			$AnimatedSprite2D.flip_h = direction.x < 0
+		else:
+			$AnimatedSprite2D.frame = 0
